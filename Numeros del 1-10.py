000d8662703d1ca00666f8521aabdb6820de6eb5
LuzMArlennyDev <L.marlenny.c@gmail.com>
2025-08-13 21:38:38 -0500

    Add basic calculator functionality with error handling and user input

---
+++ Numeros del 1-10.py
@@ -0,0 +1,23 @@
+#Numero del 1 al 10
+#Con Iteraciones
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ printMessage.py
@@ -0,0 +1,22 @@
+print("hola mundo")
+texto= input("dijite su nombre:  ") 
+edad= int(input("Which is youre age: "))
+activo= True
+print("name: {}".format(texto))
+print("age: {}".format(edad)) 
+print("age:"+str(edad))
+print(f"active? {activo}") 
+print(f"texto? {texto}")
+ 
+#  {} curly brackets
+# () round brackets¨
+# [] square brackets
+# "" quotes
+# & ampersand
+# ;semicolon
+# . period
+# : colon 
+# _ underscore
+# , comma 
+# / slash
+# ? question mark
