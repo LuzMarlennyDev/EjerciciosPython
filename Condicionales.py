7e7820bb00a8776ee76f249dbcb495fc275bc4b8
LuzMArlennyDev <L.marlenny.c@gmail.com>
2025-10-25 21:24:27 -0500

    Add basic calculator and product sales functionality with discounts

---
+++ Condicionales.py
@@ -0,0 +1,10 @@
+n1=int (input("Number one:"))
+print("n1: "+ str(n1))
+n2=int (input("Number two:"))
+print("n2: "+ str(n2))
+if(n1>5):t=n1+n2
+else:t=n1*n2
+print("total: "+ str(t))
+
+texto= input ("Hola soy tu calculadora, como estas")
+print("Hola soy tu calculadora, como estas: {}".format(texto))
